b6cc11de6e48c37be64e4bdad0ecffa89a4ea7ac
manufasa1996 <84535278+manufasa1996@users.noreply.github.com>
2021-06-16 11:24:31 -0500

    Create Style.css - login.php
    incluyendo estilos al login

---
+++ Style.css - login.php
@@ -0,0 +1,42 @@
+body {
+	
+	margin: 0;
+	padding:0;
+	width: 50%;
+	height: 50%;
+	position: absolute;
+
+}
+
+.contenedor{
+	position: absolute;
+	width: 40%;
+	height: 40vh;
+	background: #0000FF;
+    border-radius: 30%;
+    box-sizing: border-box;
+    align-content: center;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.usuario{
+
+}
+.subcontenedor{
+	bottom:0;
+	left:0;
+	position: absolute;
+	align-content: center;
+	margin: left;
+	align-content: center;
+	width: 200%;
+	height: 60%;
+	
+
+
+}
+.value
+{
+	background:#2ECCFA;
+}
